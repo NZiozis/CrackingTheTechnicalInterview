3b7703cdde0d30293fa8946b17984141647892c2
Niko Ziozis <nikolaos.ziozis@stonybrook.edu>
2018-01-29 13:23:13 -0500

    Completed the Unique question and checked it in Main

--- src/com/company/Unique.java
+++ src/com/company/Unique.java
@@ -5,19 +5,20 @@ public class Unique {
     /* without being able to use an additional data structures */
 
 
-    public boolean unique(String s){
+    public static boolean unique(String s){
+
         for (int i = 0; i < s.length(); i++){
             String current = s.substring(i, i);
 
             for (int j = i; j < s.length(); j++){
                 String tester = s.substring(j, j);
                 if (current.equals(tester)){
-                    return true;
+                    return false;
                 }
 
             }
         }
-        return false;
+        return true;
     }
 
 }
